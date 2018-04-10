d15b1d53f474040018ccaa1d4a658ccd08cf464c
fionahe <fionahe119@gmail.com>
2018-04-10 09:07:13 +0800

    add OCR Bank Card API

--- src/myservice/myservice.service.ts
+++ src/myservice/myservice.service.ts
@@ -11,6 +11,7 @@ export class MyserviceService{
 
   //https://console.faceplusplus.com.cn/documents/10069553  可查看此文档
   bankCardNo='https://api-cn.faceplusplus.com/cardpp/beta/ocrbankcard';
+  wordRecgnize = 'https://api-cn.faceplusplus.com/imagepp/v1/recognizetext';
   // getBankCardNo(image):void{
   //   //const headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
   //   let data = {
@@ -50,6 +51,29 @@ export class MyserviceService{
         let dataJson1 = JSON.parse(data.data);
         console.log('dataJson:',dataJson);
         console.log('dataJson1:',dataJson);
+        alert('success : '+data.data);
+      })
+      .catch(error => {
+        console.log(error);
+        alert(error);
+      });
+  }
+
+  getWords(image):void{
+    console.log('start ');
+    let data = {
+      'api_key': 'tW53FJsaRIkvQjmBOVyixiI2FUr2cMpD',
+      'api_secret': 'GlNLHUxJKTLa_q-A4DTZ8upG_uZRvos8',
+      'image_base64': image
+    };
+    let headers = {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    };
+    this.http.post(this.wordRecgnize, data, headers)
+      .then(data => {
+        console.log('data:',data);
+        //返回的是HTTPResponse， 怎么拆成json
+        console.log('data.data:',data.data  );
         alert('success : '+data.data);
       })
       .catch(error => {
