41d4f07ad7623107fbbd95424ec702892fcc17cf
fionahe <fionahe119@gmail.com>
2018-04-16 17:50:22 +0800

    test http post

--- src/myservice/myservice.service.ts
+++ src/myservice/myservice.service.ts
@@ -1,6 +1,7 @@
 import { Component,Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
+import {RequestOptions} from '@angular/http';
 
 @Component({
 
@@ -15,6 +16,8 @@ export class MyserviceService{
   //https://console.faceplusplus.com.cn/documents/10069553  可查看此文档
   bankCardNo='https://api-cn.faceplusplus.com/cardpp/beta/ocrbankcard';
   wordRecgnize = 'https://api-cn.faceplusplus.com/imagepp/v1/recognizetext';
+
+  //ionic http
   // getBankCardNo(image):void{
   //   //const headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
   //   let data = {
@@ -34,56 +37,77 @@ export class MyserviceService{
   //       alert(Object(error));
   //     });
   // }
+
+  //angular http
   getBankCardNo(image):void{
     console.log('start ');
-    let data = {
-      'api_key': 'tW53FJsaRIkvQjmBOVyixiI2FUr2cMpD',
-      'api_secret': 'GlNLHUxJKTLa_q-A4DTZ8upG_uZRvos8',
-      'image_base64': image
-    };
+    let formData  = new FormData();
+    formData.append('api_key','tW53FJsaRIkvQjmBOVyixiI2FUr2cMpD');
+    formData.append('api_secret','GlNLHUxJKTLa_q-A4DTZ8upG_uZRvos8');
+    //test
+    formData.append('image_url',image);
+    //proc
+    //formData.append('image_base64',image);
 
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
-    };
 
-    this.http.post(this.bankCardNo, data, httpOptions)
+
+    this.http.post(this.bankCardNo, formData, {} )
       .subscribe(data => {
         console.log('data:',data);
         //返回的是HTTPResponse， 怎么拆成json
-        console.log('data.data:',data  );
-        let dataJson = JSON.stringify(data.toString());
-        let dataJson1 = JSON.parse(data.toString());
-        console.log('dataJson:',dataJson);
-        console.log('dataJson1:',dataJson);
+        // console.log('data.data:',data  );
+        // let dataJson = JSON.stringify(data.toString());
+        // let dataJson1 = JSON.parse(data.toString());
+        // console.log('dataJson:',dataJson);
+        // console.log('dataJson1:',dataJson);
         alert('success : '+data);
       });
   }
 
   getWords(image):void{
-    console.log('start ');
-    let data = {
-      'api_key': 'tW53FJsaRIkvQjmBOVyixiI2FUr2cMpD',
-      'api_secret': 'GlNLHUxJKTLa_q-A4DTZ8upG_uZRvos8',
-      'image_base64': image
-    };
-    let headers = {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    };
+    console.log('start get words ');
 
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
-    };
 
-    this.http.post(this.wordRecgnize, data, httpOptions)
+    let formData  = new FormData();
+    formData.append('api_key','tW53FJsaRIkvQjmBOVyixiI2FUr2cMpD');
+    formData.append('api_secret','GlNLHUxJKTLa_q-A4DTZ8upG_uZRvos8');
+    //test
+    formData.append('image_url',image);
+    //proc
+    //formData.append('image_base64',image);
+
+
+    this.http.post(this.wordRecgnize, formData, {})
       .subscribe(data => {
         console.log('data:',data);
         //返回的是HTTPResponse， 怎么拆成json
         console.log('data.data:',data  );
         alert('success : '+data);
+
       });
+  }
+
+  //调不到
+  saveToUser(id:String, info:any){
+    let myurl = 'http://localhost:8182/updateuser/'+id;
+
+    console.log(myurl);
+    let formData  = new FormData();
+    const httpOptions = {
+      headers: new HttpHeaders({
+        //'Content-Type': 'application/x-www-form-urlencoded'
+         'Content-Type': 'application/json',
+        // 'Accept': 'application/json'
+        //'X-Requested-With': 'XMLHttpRequest'
+      })
+    };
+
+    formData.append('name',info);
+    let body = JSON.stringify(formData);
+
+    this.http.put(myurl, body,httpOptions)
+      .subscribe(data => {
+        console.log('data:',data);
+    });
   }
 }
