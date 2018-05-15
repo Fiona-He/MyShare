3df39dbb0f6de96cd6fcc50b44d84d2ec6051db8
Vincent <fly9879@263.net>
2018-05-15 22:20:54 +0800

    style update

--- src/pages/account/my-qrcode.ts
+++ src/pages/account/my-qrcode.ts
@@ -6,13 +6,36 @@ import {AccountPage} from "./account";
 
 @Component({
   selector: 'my-qrcode',
-  template: `<button (click)="goBack()">back</button>
-    <qr-code [value]="myuid"></qr-code>`,
+  template: `
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>
+        </ion-title>
+        <ion-buttons start>
+          <button ion-button (click)="goBack()"
+                  style="font-size: 24px;color: #59b5c0; padding-left: 10px;">
+            <ion-icon ios="md-arrow-back"></ion-icon>
+            <ion-icon name="md-close" showWhen="android, windows"></ion-icon>
+          </button>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content style="background-color: #c6e7f0;">
+      <div style="width: 100%; text-align: center; padding-top: 60px;">
+        <img style="border-radius: 40px;width: 80px;h;h;height: 80px;" [src]="avatar  || '//:0'">
+      </div>
+      <div style="width: 100%; text-align: center;">
+      <qr-code [value]="myuid"></qr-code>
+      </div>
+    </ion-content>`,
   providers:[MyserviceService]
 })
 export class MyQrcode {
 
   loader:any;
+  name: any;
+  avatar: any;
+  uid: any;
 
   myuid:any;
   constructor(public navCtrl: NavController,
@@ -21,12 +44,15 @@ export class MyQrcode {
               private myserviceService:MyserviceService
   ) {
     this.myuid = this.auth.currentUserId;
+    this.name = this.auth.currentUserDisplayName;
+    this.avatar = this.auth.currentUserPhotoURL;
+    this.uid = this.auth.currentUserId;
   }
 
   ngOnInit() {}
 
   goBack() {
-    this.navCtrl.push(AccountPage);
+    this.navCtrl.pop();
   }
 
 
