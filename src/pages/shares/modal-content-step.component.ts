62b73c67a1c2cb09502a0dcfde544a0f9cc61cde
Vincent <fly9879@263.net>
2018-05-19 17:59:38 +0800

    增加消費項目選擇

--- src/pages/shares/modal-content-step.component.ts
+++ src/pages/shares/modal-content-step.component.ts
@@ -1,8 +1,9 @@
-import {NavParams, Platform, ViewController} from 'ionic-angular';
+import {NavParams, Platform, ViewController,AlertController} from 'ionic-angular';
 import {Component} from '@angular/core';
 import {ShareService} from '../../myservice/share.service';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {AuthService} from '../core/auth.service';
+
 
 @Component({
   templateUrl:'./modal-content-step.component.html',
@@ -10,9 +11,9 @@ import {AuthService} from '../core/auth.service';
 export class ModalContentStepComponent {
   status:any;
   projectid: any;
-  amount: any;
-  amount1: any;
-  amount2: any;
+  amount: number[]=[0,0,0];
+  baselist:{label:any,value:any}[] = [];
+  list: {id:any;label:any,value:number}[] = [];
   fieldvalue1stForm: FormGroup;
   fieldvalue2ndForm: FormGroup;
   fieldvalue3rdForm: FormGroup;
@@ -22,6 +23,7 @@ export class ModalContentStepComponent {
               public params: NavParams,
               private fb: FormBuilder,
               public auth: AuthService,
+              public alertCtrl: AlertController,
               public viewCtrl: ViewController,
               private shareService:ShareService) {
     this.status = this.params.get('status');
@@ -71,6 +73,11 @@ export class ModalContentStepComponent {
       'createby': [this.auth.currentUserId,[]],
       'status': ['1',[]],
     });
+    this.baselist.push({label:'枸杞養生吐司 10.50',value:'10.50'});
+    this.baselist.push({label:'葡萄吐司 13.00',value:'13.00'});
+    this.baselist.push({label:'流心芝士包 13.00',value:'13.00'});
+    this.baselist.push({label:'可口可樂 6.50',value:'6.50'});
+
   }
 
   commit1st(){
@@ -87,12 +94,53 @@ export class ModalContentStepComponent {
     console.log(event);
     if(event != undefined)
     {
-    this.amount1 = event/2;
-    console.log(this.amount1);
+      this.amount[1] = event/2;
+      this.amount[2] = event/2;
     }
   }
 
   dismiss() {
     this.viewCtrl.dismiss();
+  }
+
+  showCheckbox(id) {
+    let alert = this.alertCtrl.create();
+    alert.setTitle('請選擇消費項目');
+
+    for(var i =0; i < this.baselist.length; i++)
+    {
+      alert.addInput({
+        type: 'checkbox',
+        label: this.baselist[i].label,
+        value: ''+i,
+        checked: false
+      });
+    }
+
+    alert.addButton('Cancel');
+    alert.addButton({
+      text: 'Okay',
+      handler: data => {
+        console.log(data);
+        for(var j=data.length-1; j>=0; j--)
+        {
+        console.log('Checkbox data:', data[j]);
+        this.list.push({id:id,label:this.baselist[data[j]].label,value:this.baselist[data[j]].value});
+        this.amount[id]= this.amount[id] + parseFloat(this.baselist[data[j]].value);
+        this.baselist.splice(data[j],1);
+        console.log(this.list);
+        console.log(this.baselist);
+        }
+      }
+    });
+    alert.present();
+  }
+
+  removeItem(label,value,id,index){
+    this.baselist.push({label:label, value:value})
+    this.amount[id] = this.amount[id] - parseFloat(value);
+    console.log(this.list);
+    console.log(index);
+    this.list.splice(index,1);
   }
 }
